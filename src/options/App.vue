bbc3e7342a1e42db2ee15ff5ca81653e9b4ee9fc
longvoxxx <long.vo@hdwebsoft.co>
2019-07-19 16:47:33 +0700

    init commit

---
+++ src/options/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>Todolist
+    <Todos />
+  </p>
+  
+</template>
+
+<script>
+import Todos from '../components/todoList';
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Todos,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
